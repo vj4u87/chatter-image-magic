63e5d2ca7c19995cc922cc3f219a2d35c322f708
vijay.swamidass <vijay.swamidass@cb714bf1-46ef-9a43-8a1c-dfdecaffb42b>
2011-05-24 00:48:28 +0000

    Download images to files.

--- trunk/src/ChatterProfileImage.java
+++ trunk/src/ChatterProfileImage.java
@@ -1,16 +1,38 @@
 
 public class ChatterProfileImage implements Downloadable {
 	
-	public ChatterProfileImage(String smallPhotoUrl, String fullPhotoUrl) {
-		
+	String smallPhotoUrl;
+	String fullPhotoUrl;
+	String userId;
+	
+	public ChatterProfileImage(String s, String f, String id) {
+		smallPhotoUrl = s;
+		fullPhotoUrl = f;
+		userId = id;
+	}
+	
+	public String getFileName() {
+		return userId;
 	}
 	
 	public String getUrl() {
+		return smallPhotoUrl;
+	}
+	
+	public String getUserId() {
+		return null;
+	}
+	
+	public String getUsername() {
+		return null;
+	}
+	
+	public String getFullName() {
 		return null;
 	}
 	
 	public String getSmallPhotoUrl() {
-		return null;
+		return smallPhotoUrl;
 	}
 	
 	public String getFullPhotoUrl() {
